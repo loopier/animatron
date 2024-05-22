c2f670c32099a56c89a89bb80d87480838b7e6be
loopier <rogerpibernat@gmail.com>
2024-05-22 17:15:26 +0200

    Disable  posting command results to post window
    It needs another approach. Currently it copies all the text in the
    window to append the message and substitutes the old version for the new
    one.
    Obviously that's not desirable.

--- Main.gd
+++ Main.gd
@@ -193,7 +193,7 @@ func evalCommand(cmdArray: Array, sender: String) -> Status:
 	
 	# post and reply result
 	_on_command_finished(result, sender)
-	if result.msg.length() > 0: post(result.msg)
+	#if result.msg.length() > 0: post(result.msg)
 	return result
 
 ## Executes a [param command] described in a [CommandDescription], with the given [param args].
