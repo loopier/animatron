9d91a6bfc86ad6fabaf98f3bfe414c2eac7178a6
loopier <rogerpibernat@gmail.com>
2024-11-07 12:16:17 +0100

    Move `for` loop to a separate thread

--- CommandInterface.gd
+++ CommandInterface.gd
@@ -207,7 +207,9 @@ func defineCommand(args: Array) -> Status:
 	return Status.ok([commandName, commandVariables, subCommands], "Added command def: %s %s %s" % [commandName, commandVariables, subCommands])
 
 func forCommand(args: Array) -> Status:
-	var cmds: Array = ocl._for(args);
+	var cmds: Array = ocl._for(args)
+	var task_callable = Callable(ocl, "_for").bind(args)
+	var task_id = WorkerThreadPool.add_task(task_callable)
 	return commandManager.evalCommands(cmds, "CommandInterface")
 	
 ## Load commands from a file and return an array
