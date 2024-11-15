f54f1e0dde483ca64060b94ad15cbe0879fd6083
loopier <rogerpibernat@gmail.com>
2024-11-15 17:08:52 +0100

    Add `/x` `/y` and `/size/xy` properties
    Allows to use them with `/tween`.

--- MetaNode.gd
+++ MetaNode.gd
@@ -14,14 +14,25 @@ var opacity: float = 1.0:
 		CommandInterface.setImageShaderUniform(animNode, "uAlpha", value as float)
 	get: return opacity
 
-var size: Vector2 = Vector2(1,1):
-	set(value):
-		scale = value
+var size: float = 1.0:
+	set(value): set_scale(Vector2(value,value))
+	get: return get_scale().x
+
+var size_xy: Vector2 = Vector2(1,1):
+	set(value): scale = value
 	get: return scale
 
 var angle: float = 0.0:
 	set(value): set_rotation_degrees(value)
 	get: return get_rotation_degrees()
+
+var x: float:
+	set(value): set_position(Vector2(value, get_position().y))
+	get: return get_position().x
+
+var y: float:
+	set(value): set_position(Vector2(value, get_position().y))
+	get: return get_position().y
 
 @onready var animNode : AnimatedSprite2D = $Animation
 
