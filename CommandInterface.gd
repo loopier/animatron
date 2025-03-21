282670cc2a55dc9974b188ab08c71f4389152650
loopier <rogerpibernat@gmail.com>
2025-03-21 11:26:51 +0100

    Add `/unix/cmd`

--- CommandInterface.gd
+++ CommandInterface.gd
@@ -142,6 +142,7 @@ var coreCommands: Dictionary = {
 	# utils
 	"/rand": CommandDescription.new(randCmdValue, "cmd:s actor:s min:f max:f", "Send a CMD to an ACTOR with a random value between MIN and MAX. If a wildcard is used, e.g. `bl*`, all ACTORs with with a name that begins with `bl` will get a different value. *WARNING: This only works with single-value commands.*", Flags.asArray(true)),
 	"/tween": CommandDescription.new(tweenActorProperty, "dur:f transition:s property:s actor:s value:f", "Tweens a PROPERTY of an ACTOR between the current value and final VALUE in a span of time equal to DURation, in seconds. The TRANSITION must be one of: linear, sine, quint, quart, quad, expo, elastic, cubic, circ, bounce, back and spring.\nSee also: <<_tween_loop,/tween/loop>>.", Flags.asArray(true)),
+	"/unix/cmd": CommandDescription.new(unixCmd, "cmd:s", "Execute a unix command and get the output in the console.", Flags.asArray(true)),
 	# Node
 	"/flip/v": CommandDescription.new(toggleAnimationProperty, "actor:s", "Flip/ ACTOR vertically.", Flags.gdScript()),
 	"/flip/h": CommandDescription.new(toggleAnimationProperty, "actor:s", "Flip ACTOR horizontally.", Flags.gdScript()),
@@ -1501,6 +1502,15 @@ func tweenActorProperty(args: Array) -> Status:
 		tween.set_trans(transitionType)
 		tween.tween_property(node, property, value, dur)
 	return Status.ok()
+
+func unixCmd(args: Array) -> Status:
+	var output := []
+	var cmd = args[0]
+	args = args.slice(1)
+	var exit_code := OS.execute(cmd, args, output, true)
+	if exit_code == -1: return Status.error("\n".join(output))
+	post(output)
+	return Status.ok(output, "\n".join(output))
 
 func getTransitionType(transition: String) -> int:
 	match transition:
