8d0d7a9892d654f9679dce0f1882c2382beb9da2
loopier <rogerpibernat@gmail.com>
2025-04-03 15:49:02 +0200

    Fix help comment

--- CommandInterface.gd
+++ CommandInterface.gd
@@ -141,8 +141,8 @@ var coreCommands: Dictionary = {
 	"/midi/cc/free": CommandDescription.new(freeMidi, "channel:i [num:i]", "Remove a cmd from the event.", Flags.gdScript()),
 	"/midi/free": CommandDescription.new(clearMidi, "", "Remove all commands from MIDI events."),
 	# utils
-	"/rand": CommandDescription.new(randCmdValue, "cmd:s actor:s min:f max:f", "Send a CMD to an ACTOR with a random value between MIN and MAX. If a wildcard is used, e.g. `bl*`, all ACTORs with with a name that begins with `bl` will get a different value. *WARNING: This only works with single-value commands.*", Flags.asArray(true)),
-	"/choose": CommandDescription.new(chooseCmdValue, "cmd:s actor:s values:...", "Send a CMD to an ACTOR with one of the VALUES chosen randomly. If a wildcard is used, e.g. `bl*`, all ACTORs with with a name that begins with `bl` will get a different value. *WARNING: This only works with single-value commands.*", Flags.asArray(true)),
+	"/rand": CommandDescription.new(randCmdValue, "cmd:s actor:s min:f max:f", "Send a CMD to an ACTOR with a random value between MIN and MAX. If a wildcard is used, e.g. `bl*`, all ACTORs with a name that begins with `bl` will get a different value. *WARNING: This only works with single-value commands.*", Flags.asArray(true)),
+	"/choose": CommandDescription.new(chooseCmdValue, "cmd:s actor:s values:...", "Send a CMD to an ACTOR with one of the VALUES chosen randomly. If a wildcard is used, e.g. `bl*`, all ACTORs with a name that begins with `bl` will get a different value. *WARNING: This only works with single-value commands.*", Flags.asArray(true)),
 	"/tween": CommandDescription.new(tweenActorProperty, "dur:f transition:s property:s actor:s value:f", "Tweens a PROPERTY of an ACTOR between the current value and final VALUE in a span of time equal to DURation, in seconds. The TRANSITION must be one of: linear, sine, quint, quart, quad, expo, elastic, cubic, circ, bounce, back and spring.\nSee also: <<_tween_loop,/tween/loop>>.", Flags.asArray(true)),
 	"/unix/cmd": CommandDescription.new(unixCmd, "cmd:s", "Execute a unix command and get the output in the console.", Flags.asArray(true)),
 	# Node
