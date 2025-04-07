203c69a6963ec36ba1db85d6e523c3ba2408813a
loopier <rogerpibernat@gmail.com>
2025-04-07 16:35:18 +0200

    Disable syntax highlighting
    Plain white feels better for now.

--- CodeEdit.gd
+++ CodeEdit.gd
@@ -14,6 +14,10 @@ signal font_size_changed(object: CodeEdit)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#setHighlighting()
+	pass
+
+func setHighlighting():
 	hl.add_color_region("#", "", Color(0.447059, 0.462745, 0.498039), true) # comment
 	hl.add_color_region("/", " ", Color(0.95309376716614, 0.42354467511177, 0.50218969583511)) # osc address
 	hl.add_color_region("$", " ", Color(0.97654688358307, 0.68245977163315, 0.44307622313499)) # variable
